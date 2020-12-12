19488c2aa3a492696ee3e5763ea451a5da7c3cfd
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-13 05:45:35 +0900

    Improve async implementation magic

--- conf.d/fly.fish
+++ conf.d/fly.fish
@@ -1,10 +1,9 @@
-function _fly_set_path --on-variable PWD
-    set --global _fly_path (string replace ~ \~ $PWD | string replace --all --regex -- "/?.+/" "")
-end
+set --global _fly_start
+set --global _fly_color_error (set_color --bold $fish_color_error)
+set --global _fly_color_accent (set_color $fish_color_command)
+set --global _fly_color_normal (set_color normal)
 
 function _fly_uninstall --on-event fly_uninstall
-    set --erase _fly_path
+    set --names | string replace --filter --regex "^_fly_" -- "set --erase _fly_" | source
     functions --erase (functions --all | string match --entire --regex "^_fly_")
 end
-
-_fly_set_path
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,21 +1,45 @@
+set --global _fly_git_status _fly_git_status_$fish_pid
+
 function fish_prompt
-    test "$pipestatus" = 0 || set --local color (set_color --bold $fish_color_error)
-    echo -n "$_fly_path $color% "(set_color normal)
+    test "$pipestatus" = 0 || set --local error $_fly_color_error
+    echo -n "$_fly_color_accent$_fly_pwdinfo$_fly_color_normal $$_fly_git_status$error%$_fly_color_normal "
+
+    set --query _fly_start && \
+        set --erase _fly_start && _fly_set_pwdinfo && _fly_fish_prompt
 end
 
-function _fly_on_fish_prompt --on-event fish_prompt
-    fish -c '
-        set --local branch (
+function _fly_set_pwdinfo --on-variable PWD
+    set --query fish_prompt_pwd_dir_length || set --local fish_prompt_pwd_dir_length 1   
+    set --global _fly_pwdinfo (
+        string replace ~ \~ $PWD | \
+        string replace --all --regex -- "(\.?[^/]{$fish_prompt_pwd_dir_length})[^/]*/" \$1/
+    )
+end
+
+function _fly_git_status --on-variable $_fly_git_status
+    commandline --function repaint
+end
+
+function _fly_fish_prompt --on-event fish_prompt
+    fish --command "    
+        if not set --local branch (
             command git symbolic-ref --short HEAD 2>/dev/null || \
             command git describe --tags --exact-match HEAD 2>/dev/null || \
             command git rev-parse --short HEAD 2>/dev/null
-        ) || exit
+        ) 
+            set _fly_git_status_$fish_pid 
+            exit
+        end
 
-        echo -n "($branch" 
+        set info \"(\$branch\"
         ! command git diff --no-ext-diff --quiet --exit-code 2>/dev/null || \
         ! command git diff --no-ext-diff --quiet --exit-code --cached 2>/dev/null && \
-            echo -n "*"
-        echo -n ") "
-    ' &
+            set info \"\$info*\"
+
+        set --universal _fly_git_status_$fish_pid \"\$info) \"
+    " &
 end   
 
+function _fly_fish_exit --on-event fish_exit
+    set --erase $_fly_git_status
+end
