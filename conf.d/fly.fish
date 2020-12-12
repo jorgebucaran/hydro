12e08bc280aa1fe0a7c33fecb55cb0062e2da639
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-13 00:19:00 +0900

    Query functions dynamically on uninstall

--- conf.d/fly.fish
+++ conf.d/fly.fish
@@ -2,9 +2,9 @@ function _fly_set_path --on-variable PWD
     set --global _fly_path (string replace ~ \~ $PWD | string replace --all --regex -- "/?.+/" "")
 end
 
-_fly_set_path
-
-function fly_uninstall --on-event fly_uninstall
+function _fly_uninstall --on-event fly_uninstall
     set --erase _fly_path
-    functions --erase _fly_set_path fly_uninstall _fly_on_fish_prompt
+    functions --erase (functions --all | string match --entire --regex "^_fly_")
 end
+
+_fly_set_path
