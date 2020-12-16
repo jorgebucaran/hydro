1e6352ffe271a6e5266482588dc3a7fd4728e17b
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-16 22:43:08 +0900

    Prettier

--- conf.d/fly.fish
+++ conf.d/fly.fish
@@ -1,9 +1,10 @@
+set --global _fly_git_status _fly_git_status_$fish_pid
 set --global _fly_init
 set --global _fly_color_dim (set_color --dim)
-set --global _fly_color_base (set_color $fish_color_status)
+set --global _fly_color_base (set_color $fish_color_command)
 set --global _fly_color_error (set_color $fish_color_error)
 set --global _fly_color_reset (set_color normal)
-set --global _fly_prompt_symbol %
+set --global _fly_symbol_prompt ❯
 
 function _fly_uninstall --on-event fly_uninstall
     set --names | string replace --filter --regex "^_fly_" -- "set --erase _fly_" | source
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,22 +1,21 @@
-set --global _fly_git_status _fly_git_status_$fish_pid
-
 function fish_prompt
     set --local last_status $pipestatus
     set --query _fly_init && set --erase _fly_init && _fly_fish_prompt && _fly_set_pwdinfo
 
     test (math +$last_status) = 0 \
-        && set --local prompt $_fly_prompt_symbol \
-        || set --local prompt "$_fly_color_error"[(string join "$_fly_color_dim|$_fly_color_reset$_fly_color_error" $last_status)]$_fly_color_reset
+        && set --local prompt $_fly_color_base$_fly_symbol_prompt \
+        || set --local prompt "$_fly_color_error"[(
+            string join "$_fly_color_dim|$_fly_color_reset$_fly_color_error" $last_status
+        )]"$_fly_color_reset"
 
-    echo -n "$_fly_pwdinfo$_fly_color_reset $$_fly_git_status$_fly_color_reset$prompt "
+    echo -n "$_fly_color_base$_fly_pwdinfo $$_fly_git_status$prompt$_fly_color_reset "
 end
 
 function _fly_set_pwdinfo --on-variable PWD
     set --query fish_prompt_pwd_dir_length || set --local fish_prompt_pwd_dir_length 1
     set --global _fly_pwdinfo (
         string replace --ignore-case -- ~ \~ $PWD | \
-        string replace --all --regex -- "(\.?[^/]{$fish_prompt_pwd_dir_length})[^/]*/" \$1/ | \
-        string replace --regex "(~|/)" "$_fly_color_base\$1$_fly_color_reset"
+        string replace --all --regex -- "(\.?[^/]{$fish_prompt_pwd_dir_length})[^/]*/" \$1/
     )
 end
 
@@ -32,21 +31,24 @@ function _fly_fish_prompt --on-event fish_prompt
             command git rev-parse --short HEAD 2>/dev/null
         ) && set $_fly_git_status && exit
 
-        command git rev-list --count --left-right @{upstream}...HEAD 2>/dev/null | read behind ahead
-
+        command git rev-list --count --left-right @{upstream}...@ 2>/dev/null | read behind ahead
+        
         switch \"\$behind \$ahead\"
             case \"\" \"0 0\"
             case \"0 *\"
-                set upstream \" $_fly_color_base↑$_fly_color_reset\$ahead\"
+                set upstream \" ↑\$ahead\"
             case \"* 0\"
-                set upstream \" $_fly_color_base↓$_fly_color_reset\$behind\"
+                set upstream \" ↓\$behind\"
             case \*
-                set upstream \" $_fly_color_base↑$_fly_color_reset\$ahead$_fly_color_base↓$_fly_color_reset\$behind\"
+                set upstream \" ↑\$ahead ↓\$behind\"
         end
 
-        ! git diff-index --quiet HEAD || count (command git ls-files --others --exclude-standard) >/dev/null && \
-            set --universal $_fly_git_status \"(\$branch$_fly_color_base*$_fly_color_reset\$upstream) \" || \
-            set --universal $_fly_git_status \"(\$branch\$upstream) \"
+        ! git diff-index --quiet HEAD || \
+        count (command git ls-files --others --exclude-standard) >/dev/null && set state \*
+
+        set --universal $_fly_git_status \"$_fly_color_dim\$branch\$state\$upstream$_fly_color_reset \"
+
+        command git fetch 2>/dev/null &
     " &
 end   
 
