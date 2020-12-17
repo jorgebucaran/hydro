4a5fbafaadeb45e412c15806832edb0d65fa9171
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-17 17:19:00 +0900

    Add fish_mode_prompt / support vi mode

---
+++ functions/fish_mode_prompt.fish
@@ -0,0 +1,21 @@
+function fish_mode_prompt
+    if test $fish_key_bindings != fish_default_key_bindings
+        set --local vi_mode_color
+        set --local vi_mode_symbol
+        switch $fish_bind_mode
+            case default
+                set vi_mode_color (set_color $fish_color_selection)
+                set vi_mode_symbol N
+            case insert
+                set vi_mode_color (set_color $fish_color_selection)
+                set vi_mode_symbol I
+            case replace_one
+                set vi_mode_color (set_color --reverse $fish_color_selection)
+                set vi_mode_symbol R
+            case visual
+                set vi_mode_color (set_color --reverse $fish_color_selection)
+                set vi_mode_symbol V
+        end
+        echo -n "$vi_mode_color $vi_mode_symbol $_fly_color_reset "
+    end
+end
