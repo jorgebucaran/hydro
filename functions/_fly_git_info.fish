a1a6f89c00f4487349e8a484b895d88f31f347ab
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-18 18:09:38 +0900

    Update ahead/behind count after git fetch in async loop

--- functions/_fly_git_info.fish
+++ functions/_fly_git_info.fish
@@ -1,28 +1,32 @@
 function _fly_git_info --on-event fish_prompt
-    fish --command "
+    fish --private --command "
+        command kill $_fly_last_pid 2>/dev/null
+
         ! set branch (
             command git symbolic-ref --short HEAD 2>/dev/null || \
             command git describe --tags --exact-match HEAD 2>/dev/null || \
             command git rev-parse --short HEAD 2>/dev/null
-        ) && set $_fly_git_info && exit
-
-        command git rev-list --count --left-right @{upstream}...@ 2>/dev/null | read behind ahead
-
-        switch \"\$behind \$ahead\"
-            case \" \" \"0 0\"
-            case \"0 *\"
-                set upstream \" ↑\$ahead\"
-            case \"* 0\"
-                set upstream \" ↓\$behind\"
-            case \*
-                set upstream \" ↑\$ahead ↓\$behind\"
-        end
+        ) && set --universal $_fly_git_info && exit
 
         ! git diff-index --quiet HEAD || \
         count (command git ls-files --others --exclude-standard) >/dev/null && set state \*
+            
+        for step in fetch done
+            command git rev-list --count --left-right @{upstream}...@ 2>/dev/null | read behind ahead
 
-        set --universal $_fly_git_info \"$_fly_color_bold\$branch$_fly_color_reset$_fly_color_base\$state\$upstream \"
+            switch \"\$behind \$ahead\"
+                case \" \" \"0 0\"
+                case \"0 *\"
+                    set upstream \" ↑\$ahead\"
+                case \"* 0\"
+                    set upstream \" ↓\$behind\"
+                case \*
+                    set upstream \" ↑\$ahead ↓\$behind\"
+            end
 
-        command git fetch --no-tags 2>/dev/null &
+            set --universal $_fly_git_info \"$_fly_color_bold\$branch$_fly_color_reset$_fly_color_base\$state\$upstream \"
+            test \$step = fetch && command git fetch --no-tags 2>/dev/null
+        end
     " &
+    set --global _fly_last_pid (jobs --last --pid)
 end
