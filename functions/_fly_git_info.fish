019d06f4e73192bbc255bab42a16cfd64cdb56eb
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-19 02:00:09 +0900

    Refactor _fly_git_info

--- functions/_fly_git_info.fish
+++ functions/_fly_git_info.fish
@@ -1,17 +1,17 @@
 function _fly_git_info --on-event fish_prompt
-    fish --private --command "
-        command kill $_fly_last_pid 2>/dev/null
+    command kill $_fly_last_pid 2>/dev/null
 
+    fish --private --command "
         ! set branch (
             command git symbolic-ref --short HEAD 2>/dev/null || \
             command git describe --tags --exact-match HEAD 2>/dev/null || \
             command git rev-parse --short HEAD 2>/dev/null
-        ) && set --universal $_fly_git_info && exit
+        ) && set $_fly_git_info && exit
 
         ! git diff-index --quiet HEAD || \
-        count (command git ls-files --others --exclude-standard) >/dev/null && set state \*
+        count (command git ls-files --others --exclude-standard) >/dev/null && set state \" •\"
             
-        for step in fetch done
+        for step in fetch exit
             command git rev-list --count --left-right @{upstream}...@ 2>/dev/null | read behind ahead
 
             switch \"\$behind \$ahead\"
@@ -25,6 +25,7 @@ function _fly_git_info --on-event fish_prompt
             end
 
             set --universal $_fly_git_info \"$_fly_color_bold\$branch$_fly_color_reset$_fly_color_base\$state\$upstream \"
+
             test \$step = fetch && command git fetch --no-tags 2>/dev/null
         end
     " &
