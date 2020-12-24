3898757194d99039bd5a5b5384c23f924cb30856
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-25 00:25:37 +0900

    Ignore fish_prompt_pwd_dir_length, and hardcode length=1

--- conf.d/hydro.fish
+++ conf.d/hydro.fish
@@ -1,20 +1,19 @@
-set --global _hydro_symbol_prompt ❱
-set --global _hydro_symbol_git_dirty •
-set --global _hydro_symbol_git_ahead ↑
-set --global _hydro_symbol_git_behind ↓
-
-set --global _hydro_color_error (set_color $fish_color_error)
-set --global _hydro_color_base (set_color $fish_color_command)
-set --global _hydro_color_pwd (set_color $fish_color_command)
 set --global _hydro_git_info _hydro_git_info_$fish_pid
 
+set --query hydro_symbol_prompt || set --global hydro_symbol_prompt ❱
+set --query hydro_symbol_git_dirty || set --global hydro_symbol_git_dirty •
+set --query hydro_symbol_git_ahead || set --global hydro_symbol_git_ahead ↑
+set --query hydro_symbol_git_behind || set --global hydro_symbol_git_behind ↓
+set --query hydro_color_error || set --global hydro_color_error $fish_color_error
+set --query hydro_color_base || set --global hydro_color_base $fish_color_command
+set --query hydro_color_pwd || set --global hydro_color_pwd $fish_color_command
+
 function _hydro_pwd_info --on-variable PWD
-    set --query fish_prompt_pwd_dir_length || set --local fish_prompt_pwd_dir_length 1
     set --local base (git rev-parse --show-toplevel 2>/dev/null | string replace --all --regex -- "^.*/" "")
     set --global _hydro_pwd_info (
         string replace --ignore-case -- ~ \~ $PWD | \
         string replace -- "/$base/" /:/ | \
-        string replace --regex --all -- "(\.?[^/]{$fish_prompt_pwd_dir_length})[^/]*/" \$1/ | \
+        string replace --regex --all -- "(\.?[^/]{1})[^/]*/" \$1/ | \
         string replace -- : "$base" | \
         string replace --regex -- "(?!^~\$)([^/]*)\$" "\x1b[1m\$1\x1b[22m" | \
         string replace --regex --all -- / "\x1b[2m/\x1b[22m"
@@ -36,7 +35,7 @@ function _hydro_git_info --on-event fish_prompt
         ) && set $_hydro_git_info && exit
 
         ! git diff-index --quiet HEAD 2>/dev/null || \
-        count (command git ls-files --others --exclude-standard) >/dev/null && set state $_hydro_symbol_git_dirty
+        count (command git ls-files --others --exclude-standard) >/dev/null && set state $hydro_symbol_git_dirty
 
         for step in fetch exit
             command git rev-list --count --left-right @{upstream}...@ 2>/dev/null | read behind ahead
@@ -44,11 +43,11 @@ function _hydro_git_info --on-event fish_prompt
             switch \"\$behind \$ahead\"
                 case \" \" \"0 0\"
                 case \"0 *\"
-                    set upstream \" $_hydro_symbol_git_ahead\$ahead\"
+                    set upstream \" $hydro_symbol_git_ahead\$ahead\"
                 case \"* 0\"
-                    set upstream \" $_hydro_symbol_git_behind\$behind\"
+                    set upstream \" $hydro_symbol_git_behind\$behind\"
                 case \*
-                    set upstream \" $_hydro_symbol_git_ahead\$ahead $_hydro_symbol_git_behind\$behind\"
+                    set upstream \" $hydro_symbol_git_ahead\$ahead $hydro_symbol_git_behind\$behind\"
             end
 
             set --universal $_hydro_git_info \"\$branch\$state\$upstream \"
--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,8 +1,12 @@
 function fish_prompt
     set --local last_status $pipestatus
-    test (math +$last_status) = 0 \
-        && set --local prompt "\x1b[2m$_hydro_symbol_prompt\x1b[22m" \
-        || set --local prompt "$_hydro_color_error"[(string join "\x1b[2m|\x1b[22m" $last_status)]
+    set --local color_pwd (set_color $hydro_color_pwd)
+    set --local color_base (set_color $hydro_color_base)
+    set --local color_error (set_color $hydro_color_error)
 
-    string unescape "$_hydro_color_pwd$_hydro_pwd_info $_hydro_color_base$$_hydro_git_info$_hydro_cmd_duration$prompt\x1b[0m "
+    test (math +$last_status) = 0 \
+        && set --local prompt "\x1b[2m$hydro_symbol_prompt\x1b[22m" \
+        || set --local prompt "$color_error"[(string join "\x1b[2m|\x1b[22m" $last_status)]
+
+    string unescape "$color_pwd$_hydro_pwd_info $color_base$$_hydro_git_info$_hydro_cmd_duration$prompt\x1b[0m "
 end
