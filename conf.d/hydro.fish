517ac40d28d79dc49d6b3f49abc835fadff6aada
Jorge Bucaran <mail@jorgebucaran.com>
2020-12-27 01:22:29 +0900

    Quote

--- conf.d/hydro.fish
+++ conf.d/hydro.fish
@@ -58,7 +58,7 @@ function _hydro_prompt --on-event fish_prompt
         test -z \"$$_hydro_git\" && set --universal $_hydro_git \"\$branch \"
 
         ! command git diff-index --quiet HEAD 2>/dev/null || \
-        count (command git ls-files --others --exclude-standard) >/dev/null && set state $hydro_symbol_git_dirty
+        count (command git ls-files --others --exclude-standard) >/dev/null && set state \"$hydro_symbol_git_dirty\"
 
         for step in fetch exit
             command git rev-list --count --left-right @{upstream}...@ 2>/dev/null | read behind ahead
