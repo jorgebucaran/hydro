d5fa53e1251cf800cc71737948e915669db9aeec
Bob Soppe <bobsoppe@users.noreply.github.com>
2022-02-21 12:10:11 +0100

    Fix multiline prompt not working

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt -d Hydro
-    echo "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_prompt$hydro_color_normal "
+    echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_prompt$hydro_color_normal "
 end
