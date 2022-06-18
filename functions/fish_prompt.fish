d08161e1538a32a37fbc5034560cce544a4cbc9c
mattmc3 <mattmc3@gmail.com>
2022-06-17 21:20:24 -0400

    Add pre/post prompt feature

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,3 +1,3 @@
 function fish_prompt --description Hydro
-    echo -e "$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_prompt$hydro_color_normal "
+    echo -e "$hydro_pre_prompt$_hydro_color_pwd$_hydro_pwd$hydro_color_normal $_hydro_color_git$$_hydro_git$hydro_color_normal$_hydro_color_duration$_hydro_cmd_duration$hydro_color_normal$_hydro_prompt$hydro_color_normal$hydro_post_prompt"
 end
