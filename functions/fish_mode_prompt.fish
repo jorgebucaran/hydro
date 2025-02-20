26ce0c203b6927963c3786c38fddaf19ef31cbda
Matt Dembiczak <matt@mojadem.dev>
2025-02-20 11:34:20 -0500

    update `fish_mode_prompt.fish` to use `set_color normal`

--- functions/fish_mode_prompt.fish
+++ functions/fish_mode_prompt.fish
@@ -16,6 +16,6 @@ function fish_mode_prompt
                 set vi_mode_color (set_color $fish_color_match)
                 set vi_mode_symbol V
         end
-        echo -e "$vi_mode_color $vi_mode_symbol \x1b[0m "
+        echo -e "$vi_mode_color $vi_mode_symbol $(set_color normal) "
     end
 end
